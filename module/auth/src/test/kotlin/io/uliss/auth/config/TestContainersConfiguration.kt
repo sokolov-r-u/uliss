15a935c6e7258ed366d52896ed6dfeca6e42342d
Raman Sakalou <raman.sakalou@accuristech.com>
2026-06-08 17:38:40 +0700

    update test dependency init

--- module/auth/src/test/kotlin/io/uliss/auth/config/TestContainersConfiguration.kt
+++ module/auth/src/test/kotlin/io/uliss/auth/config/TestContainersConfiguration.kt
@@ -4,12 +4,16 @@ import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.springframework.context.annotation.Bean
 import org.testcontainers.postgresql.PostgreSQLContainer
+import org.testcontainers.utility.DockerImageName
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestContainersConfiguration {
     @Bean
     @ServiceConnection
-    fun postgresContainer(): PostgreSQLContainer {
-        return PostgreSQLContainer("pgvector/pgvector:0.8.2-pg18-trixie")
-    }
+    fun postgresContainer(): PostgreSQLContainer = PostgreSQLContainer(
+        DockerImageName.parse(
+            "pgvector/pgvector:0.8.2-pg18-trixie"
+        )
+            .asCompatibleSubstituteFor("postgres")
+    )
 }
