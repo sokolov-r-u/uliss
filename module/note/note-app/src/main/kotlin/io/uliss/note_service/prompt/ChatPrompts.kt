2388432148382b975ed59126c42c0542455cbc5e
Raman Sakalou <sokolov.r.u@icloud.com>
2026-08-17 14:54:06 +0700

    FE [note] create chat prompt stab
    Signed-off-by: Raman Sakalou <sokolov.r.u@icloud.com>

---
+++ module/note/note-app/src/main/kotlin/io/uliss/note_service/prompt/ChatPrompts.kt
@@ -0,0 +1,7 @@
+package io.uliss.note_service.prompt
+
+object ChatPrompts {
+    const val CHAT_SYSTEM_PROMPT: String =
+        "You are a helpful assistant integrated into the Uliss notes application. " +
+                "Answer clearly and concisely."
+}
