c2305c71c0577be397ba3097c0038afffc2049db
Raman Sakalou <sokolov.r.u@icloud.com>
2026-09-22 16:11:17 +0700

    FE [web] - remove AskServices
    Signed-off-by: Raman Sakalou <sokolov.r.u@icloud.com>

--- module/note/note-app/src/main/kotlin/io/uliss/note_service/dto/AskRequest.kt
+++
@@ -1,8 +0,0 @@
-package io.uliss.note_service.dto
-
-import jakarta.validation.constraints.NotBlank
-
-data class AskRequest(
-    @field:NotBlank
-    val prompt: String,
-)
--- module/note/note-app/src/main/kotlin/io/uliss/note_service/dto/AskResponse.kt
+++
@@ -1,5 +0,0 @@
-package io.uliss.note_service.dto
-
-data class AskResponse(
-    val answer: String,
-)
